8c143c482fd525f3065ab9f4b56e8bba582cc34a
Jerry W <jerrywiltse@gmail.com>
2017-10-25 14:20:24 -0400

    initial commit

---
+++ build.py
@@ -0,0 +1,36 @@
+import platform
+from conan.packager import ConanMultiPackager, os, re
+
+    
+if __name__ == "__main__":
+    reponame_a = os.getenv("APPVEYOR_REPO_NAME","")
+    repobranch_a = os.getenv("APPVEYOR_REPO_BRANCH","")
+
+    reponame_t = os.getenv("TRAVIS_REPO_SLUG","")
+    repobranch_t = os.getenv("TRAVIS_BRANCH","")
+
+    username, repo = reponame_a.split("/") if reponame_a else reponame_t.split("/")
+    channel, version = repobranch_a.split("/") if repobranch_a else repobranch_t.split("/")
+    
+    with open("conanfile.py", "r") as conanfile:
+        contents = conanfile.read()
+        name = re.search(r'name\s*=\s*"(\S*)"', contents).groups()[0]
+    
+    os.environ["CONAN_USERNAME"] = username
+    os.environ["CONAN_CHANNEL"] = channel
+    os.environ["CONAN_REFERENCE"] = "{0}/{1}".format(name, version)
+    os.environ["CONAN_UPLOAD"]="https://api.bintray.com/conan/{0}/public-conan".format(username)
+    os.environ["CONAN_REMOTES"]="https://api.bintray.com/conan/conan-community/conan"
+    builder = ConanMultiPackager(args="--build missing")
+    builder.add_common_builds(shared_option_name="cpprestsdk:shared")
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        # XXX (uilian.ries): MTd could not link
+        if settings["compiler"] == "Visual Studio" and settings["compiler.runtime"] == "MTd":
+            continue
+        # XXX (uilian.ries): libstdc++ could not link on Linux
+        if settings["compiler"] == "clang" and platform.system() == "Linux":
+            settings["compiler.libcxx"] = "libc++"
+        filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
+    builder.run()
---
+++ conanfile.py
@@ -0,0 +1,42 @@
+from conans import ConanFile, os, tools
+
+class LibnameConan(ConanFile):
+    name = "libname"
+    version = "0.0.0"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    url = "https://github.com/bincrafters/conan-libname"
+    description = "Keep it short"
+    license = "https://github.com/someauthor/somelib/blob/master/LICENSES"
+    root = name + "-" + version
+
+    def requirements(self):
+        #use static org/channel for libs in conan-center
+        self.requires.add("OpenSSL/1.0.2l@conan/stable")
+        self.requires.add("zlib/1.2.11@conan/stable")
+        
+        #use dynamic org/channel for libs in bincrafters 
+        self.requires.add("websocketpp/0.7.0@%s/%s" % (self.user, self.channel))
+
+    def source(self):
+        source_url = "https://github.com/Microsoft/cpprestsdk"
+        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+
+    def build(self):
+
+        cmake = CMake(self)
+        cmake.definitions["BUILD_TESTS"] = False
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="*", dst="include", src="include")
+        self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
+
+    def package_info(self):
+        tools.collect_libs(self)
