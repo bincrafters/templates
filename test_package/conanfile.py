3185b7a804f1718e549702d241e781161353ee7b
SSE4 <tomskside@gmail.com>
2017-11-01 21:36:40 +0700

    - spaces

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -13,9 +13,10 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         with tools.environment_append(RunEnvironment(self).vars):
+            bin_path = os.path.join("bin", "test_package")
             if self.settings.os == "Windows":
-                self.run(os.path.join("bin","test_package"))
+                self.run(bin_path)
             elif self.settings.os == "Macos":
-                self.run("DYLD_LIBRARY_PATH=%s %s"%(os.environ.get('DYLD_LIBRARY_PATH', ''),os.path.join("bin","test_package")))
+                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
             else:
-                self.run("LD_LIBRARY_PATH=%s %s"%(os.environ.get('LD_LIBRARY_PATH', ''),os.path.join("bin","test_package")))
+                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
