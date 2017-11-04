9520dfc45f2cb639e59297d8df903e96afcad2a8
Jerry W <jerrywiltse@gmail.com>
2017-11-04 19:16:52 -0400

    moved imports

--- build.py
+++ build.py
@@ -1,17 +1,23 @@
-from conan.packager import ConanMultiPackager, os, re
+from conan.packager import ConanMultiPackager
+import os, re, platform
 
 
-def get_name_version_from_recipe():
+def get_value_from_recipe(search_string):
     with open("conanfile.py", "r") as conanfile:
         contents = conanfile.read()
-        name = re.search(r'name\s*=\s*"(\S*)"', contents).groups()[0]
-        version = re.search(r'version\s*=\s*"(\S*)"', contents).groups()[0]
-    return name, version
+        result = re.search(search_string, contents)
+    return result
+
+def get_name_from_recipe():
+    return get_value_from_recipe(r'name\s*=\s*"(\S*)"').groups()[0]
+
+def get_version_from_recipe():
+    return get_value_from_recipe(r'version\s*=\s*"(\S*)"').groups()[0]
 
 def get_default_vars():
     username = os.getenv("CONAN_USERNAME", "bincrafters")
     channel = os.getenv("CONAN_CHANNEL", "testing")
-    _, version = get_name_version_from_recipe()
+    version = get_version_from_recipe()
     return username, channel, version
 
 def is_ci_running():
@@ -31,13 +37,16 @@ def get_ci_vars():
 def get_env_vars():
     return get_ci_vars() if is_ci_running() else get_default_vars()
 
+def get_os():
+    return platform.system().replace("Darwin", "Macos")
+    
 if __name__ == "__main__":
-    name, _ = get_name_version_from_recipe()
+    name = get_name_from_recipe()
     username, channel, version = get_env_vars()
     reference = "{0}/{1}".format(name, version)
     upload = "https://api.bintray.com/conan/{0}/public-conan".format(username)
 
     builder = ConanMultiPackager(username=username, channel=channel, reference=reference, upload=upload,
                                  upload_only_when_stable=True, stable_branch_pattern="stable/*")
-    builder.add_common_builds(shared_option_name="%s:shared" % name)
+    builder.add_common_builds()
     builder.run()
