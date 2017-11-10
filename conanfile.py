90e745cfa8eba8d62977e955a232ff46d4746586
Jerry W <jerrywiltse@gmail.com>
2017-11-10 15:29:03 -0500

    add CMakeLists.txt and customizations regarding sources

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,6 @@ class LibnameConan(ConanFile):
     url = "https://github.com/bincrafters/conan-libname"
     description = "Keep it short"
     license = "https://github.com/someauthor/somelib/blob/master/LICENSES"
-    root = name + "-" + version
     #use static org/channel for libs in conan-center
     #use dynamic org/channel for libs in bincrafters
     requires = "OpenSSL/1.0.2l@conan/stable", \
@@ -24,21 +23,27 @@ class LibnameConan(ConanFile):
     def source(self):
         source_url = "https://github.com/Microsoft/cpprestsdk"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, "sources")
+        #Rename to "sources" is a convention to simplify later steps
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False
-        cmake.configure()
+        cmake.definitions["BUILD_TESTS"] = False # example
+        cmake.configure(source_dir="sources")
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE")
-        self.copy(pattern="*", dst="include", src="include")
-        self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
+        with tools.chdir("sources"):
+            self.copy(pattern="LICENSE")
+            self.copy(pattern="*", dst="include", src="include")
+            self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
+            self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
+            self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
+            self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
+            self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
 
     def package_info(self):
         tools.collect_libs(self)
+
+    
