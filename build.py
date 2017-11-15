a269d38b88c3a304c4ef710c3365b2cfe284728a
Uilian Ries <uilianries@gmail.com>
2017-11-15 11:02:08 -0200

    Add header-only template
    - Added header-only project template
    - Removed upload behavior
    - Only build using one job by platform
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -39,7 +39,7 @@ def get_env_vars():
 
 def get_os():
     return platform.system().replace("Darwin", "Macos")
-    
+
 if __name__ == "__main__":
     name = get_name_from_recipe()
     username, channel, version = get_env_vars()
@@ -47,13 +47,11 @@ if __name__ == "__main__":
     upload = "https://api.bintray.com/conan/{0}/public-conan".format(username)
 
     builder = ConanMultiPackager(
-        username=username, 
-        channel=channel, 
-        reference=reference, 
-        upload=upload,
-        remotes=upload, #while redundant, this moves bincrafters remote to position 0
-        upload_only_when_stable=True, 
-        stable_branch_pattern="stable/*")
-        
-    builder.add_common_builds(shared_option_name=name + ":shared")
+        username=username,
+        channel=channel,
+        reference=reference,
+        upload=False,
+        remotes=upload)
+
+    builder.add_common_builds()
     builder.run()
--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
 import os
 
 
@@ -11,11 +11,6 @@ class LibnameConan(ConanFile):
     url = "https://github.com/bincrafters/conan-libname"
     description = "Keep it short"
     license = "https://github.com/someauthor/somelib/blob/master/LICENSES"
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    #use static org/channel for libs in conan-center
-    #use dynamic org/channel for libs in bincrafters
     requires = "OpenSSL/1.0.2l@conan/stable", \
         "zlib/1.2.11@conan/stable", \
         "websocketpp/0.7.0@%s/%s" % (self.user, self.channel)
@@ -27,23 +22,10 @@ class LibnameConan(ConanFile):
         os.rename(extracted_dir, "sources")
         #Rename to "sources" is a convention to simplify later steps
 
-    def build(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        cmake.configure(source_dir="sources")
-        cmake.build()
-
     def package(self):
         with tools.chdir("sources"):
             self.copy(pattern="LICENSE")
             self.copy(pattern="*", dst="include", src="include")
-            self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
-            self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
 
-    def package_info(self):
-        tools.collect_libs(self)
-
-    
+    def package_id(self):
+        self.info.header_only()
