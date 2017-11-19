72fde9f38993e9ab5b83854d51e8867c9ea1071e
SSE4 <tomskside@gmail.com>
2017-11-19 17:25:37 +0700

    - spaces, pep8, shebang

--- build.py
+++ build.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from conan.packager import ConanMultiPackager
-import os, re, platform
+import os
+import re
+import platform
 
 
 def get_value_from_recipe(search_string):
@@ -8,11 +13,14 @@ def get_value_from_recipe(search_string):
         result = re.search(search_string, contents)
     return result
 
+
 def get_name_from_recipe():
     return get_value_from_recipe(r'''name\s*=\s*["'](\S*)["']''').groups()[0]
 
+
 def get_version_from_recipe():
     return get_value_from_recipe(r'''version\s*=\s*["'](\S*)["']''').groups()[0]
+
 
 def get_default_vars():
     username = os.getenv("CONAN_USERNAME", "bincrafters")
@@ -20,8 +28,10 @@ def get_default_vars():
     version = get_version_from_recipe()
     return username, channel, version
 
+
 def is_ci_running():
-    return os.getenv("APPVEYOR_REPO_NAME","") or os.getenv("TRAVIS_REPO_SLUG","")
+    return os.getenv("APPVEYOR_REPO_NAME", "") or os.getenv("TRAVIS_REPO_SLUG", "")
+
 
 def get_ci_vars():
     reponame_a = os.getenv("APPVEYOR_REPO_NAME","")
@@ -34,12 +44,15 @@ def get_ci_vars():
     channel, version = repobranch_a.split("/") if repobranch_a else repobranch_t.split("/")
     return username, channel, version
 
+
 def get_env_vars():
     return get_ci_vars() if is_ci_running() else get_default_vars()
 
+
 def get_os():
     return platform.system().replace("Darwin", "Macos")
-    
+
+
 if __name__ == "__main__":
     name = get_name_from_recipe()
     username, channel, version = get_env_vars()
@@ -47,13 +60,13 @@ if __name__ == "__main__":
     upload = "https://api.bintray.com/conan/{0}/public-conan".format(username)
 
     builder = ConanMultiPackager(
-        username=username, 
-        channel=channel, 
-        reference=reference, 
+        username=username,
+        channel=channel,
+        reference=reference,
         upload=upload,
-        remotes=upload, #while redundant, this moves bincrafters remote to position 0
-        upload_only_when_stable=True, 
+        remotes=upload,  # while redundant, this moves bincrafters remote to position 0
+        upload_only_when_stable=True,
         stable_branch_pattern="stable/*")
-        
+
     builder.add_common_builds(shared_option_name=name + ":shared")
     builder.run()
