4e69166df709bf381bce0c761285001f7049545f
Jerry W <jerrywiltse@gmail.com>
2017-12-29 17:41:12 -0500

    moved builder into shared

--- build_shared.py
+++ build_shared.py
@@ -64,4 +64,24 @@ def get_conan_vars():
 
 def get_os():
     return platform.system().replace("Darwin", "Macos")
+    
 
+def get_builder():
+    name = get_name_from_recipe()
+    username, channel, version = get_conan_vars()
+    reference = "{0}/{1}".format(name, version)
+    upload = "https://api.bintray.com/conan/{0}/public-conan".format(username)
+    remotes = os.getenv("CONAN_REMOTES", upload)
+    upload_when_stable = os.getenv("CONAN_UPLOAD_ONLY_WHEN_STABLE", 1)
+    stable_branch_pattern = os.getenv("CONAN_UPLOAD_ONLY_WHEN_STABLE", "stable/*")
+
+    builder = ConanMultiPackager(
+        username=username,
+        channel=channel,
+        reference=reference,
+        upload=upload,
+        remotes=remotes,  
+        upload_only_when_stable=upload_when_stable,
+        stable_branch_pattern=stable_branch_pattern)
+
+    return builder
