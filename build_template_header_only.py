d1613ae0643f2ff3dc39fb687832e4fadcd07c6b
Jerry W <jerrywiltse@gmail.com>
2017-12-29 19:14:58 -0500

    cosmetic refactor

--- build_template_header_only.py
+++ build_template_header_only.py
@@ -18,14 +18,16 @@ def get_module_filename():
     return get_module_name() + ".py"
     
 def get_module_url():
-    return get_module_location() + "/" + get_module_name() + ".py"
+    return get_module_location() + "/" + get_module_filename()
     
     
 def build():
+
     tools.download(get_module_url(), get_module_filename(), overwrite=True)
-    import build_shared  # pylint: disable=F0401
-        
-    package_name = build_shared.get_name_from_recipe()
+
+    module = importlib.import_module(get_module_name())
+    
+    package_name = module.get_name_from_recipe()
     
     builder = build_shared.get_builder()
 
