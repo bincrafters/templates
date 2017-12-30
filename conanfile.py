d42ca1131b877c8d15a465199a91aabed5c92766
Jerry W <jerrywiltse@gmail.com>
2017-12-30 00:45:14 -0500

    another set of cosmetic refactors

--- conanfile.py
+++ conanfile.py
@@ -10,19 +10,29 @@ class LibnameConan(ConanFile):
     version = "0.0.0"
     url = "https://github.com/bincrafters/conan-libname"
     description = "Keep it short"
-    license = "https://github.com/someauthor/somelib/blob/master/LICENSES"
-    exports_sources = ["LICENSE"]
-
+    
+    # Indicates License type of the packaged library
+    license = "MIT"
+    
+    # Packages the license for the conanfile.py
+    exports = ["LICENSE.md"]
+    
+    # Custom attributes for Bincrafters recipe conventions
+    source_folder = "source_folder"
+    
     def source(self):
         source_url = "https://github.com/libauthor/libname"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, "sources")
-        #Rename to "sources" is a convention to simplify later steps
+
+        #Rename to "source_folder" is a convention to simplify later steps
+        os.rename(extracted_dir, self.source_folder)
+
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src="sources")
-        self.copy(pattern="*", dst="include", src=os.path.join("sources", "include"))
+        include_folder = os.path.join(self.source_folder, "include")
+        self.copy(pattern="LICENSE")
+        self.copy(pattern="*", dst="include", src=include_folder)
 
     def package_id(self):
         self.info.header_only()
