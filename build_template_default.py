2f367bf45efb5d2492d66765d83016b71546ac56
Uilian Ries <uilianries@gmail.com>
2018-01-05 19:28:27 -0200

    Added shared option
    - Detect and use shared option when is present on recipe
    - Use "bincrafters" as CONAN_USERNAME when empty
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build_template_default.py
+++ build_template_default.py
@@ -11,27 +11,30 @@ def get_module_location():
     repo = os.getenv("CONAN_MODULE_REPO", "https://raw.githubusercontent.com/bincrafters/conan-templates")
     branch = os.getenv("CONAN_MODULE_BRANCH", "package_tools_modules")
     return repo + "/" + branch
-    
+
 def get_module_name():
     return "build_shared"
 
 def get_module_filename():
     return get_module_name() + ".py"
-    
+
 def get_module_url():
     return get_module_location() + "/" + get_module_filename()
-    
+
 def get_builder(args=None):
 
     tools.download(get_module_url(), get_module_filename(), overwrite=True)
 
     module = importlib.import_module(get_module_name())
-    
+
     package_name = module.get_name_from_recipe()
-    
+
     builder = module.get_builder(args)
 
-    builder.add_common_builds()
-    
+    shared_option_name = None
+    if module.is_shared():
+        shared_option_name = "%s:shared" % module.get_name_from_recipe()
+
+    builder.add_common_builds(shared_option_name=shared_option_name)
+
     return builder
-    
