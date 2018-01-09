e87496b3f7fe0b1cdbc184b0dc79714ec10e83db
theirix <theirix@gmail.com>
2018-01-09 12:43:58 +0300

    Fixed trailing spaces

--- conanfile.py
+++ conanfile.py
@@ -10,32 +10,32 @@ class LibnameConan(ConanFile):
     version = "0.0.0"
     url = "https://github.com/bincrafters/conan-libname"
     description = "Keep it short"
-    
+
     # Indicates License type of the packaged library
     license = "MIT"
-    
+
     # Packages the license for the conanfile.py
     exports = ["LICENSE.md"]
-    
+
     # Remove following lines if the target lib does not use cmake.
     exports_sources = ["CMakeLists.txt"]
-    generators = "cmake" 
-    
-    # Options may need to change depending on the packaged library. 
+    generators = "cmake"
+
+    # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    
+
     # Custom attributes for Bincrafters recipe conventions
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
-    
+
     # Use version ranges for dependencies unless there's a reason not to
     requires = (
         "OpenSSL/[>=1.0.2l]@conan/stable",
         "zlib/[>=1.2.11]@conan/stable"
     )
-        
+
     def source(self):
         source_url = "https://github.com/libauthor/libname"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
@@ -44,14 +44,14 @@ class LibnameConan(ConanFile):
         #Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self.source_subfolder)
 
-        
+
     def build(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_TESTS"] = False # example
         cmake.configure(build_folder=self.build_subfolder)
         cmake.build()
         cmake.install()
-        
+
     def package(self):
         # If the CMakeLists.txt has a proper install method, the steps below may be redundant
         # If so, you can replace all the steps below with the word "pass"
@@ -64,6 +64,6 @@ class LibnameConan(ConanFile):
         self.copy(pattern="*.so*", dst="lib", keep_path=False)
         self.copy(pattern="*.dylib", dst="lib", keep_path=False)
 
-        
+
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
