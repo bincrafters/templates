b8c50f12d7dd370e58fd47227a4698a605784ea6
Jerry W <jerrywiltse@gmail.com>
2018-01-17 10:18:13 -0500

    migrate buld.py to using bincrafters-package-tools

--- build.py
+++ build.py
@@ -1,38 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conan.packager import ConanMultiPackager
-from conans import tools
-import importlib
-import os
 
-
-def get_module_location():
-    repo = os.getenv("CONAN_MODULE_REPO", "https://raw.githubusercontent.com/bincrafters/conan-templates")
-    branch = os.getenv("CONAN_MODULE_BRANCH", "package_tools_modules")
-    return repo + "/" + branch
-
-
-def get_module_name():
-    return os.getenv("CONAN_MODULE_NAME", "build_template_default")
-
-
-def get_module_filename():
-    return get_module_name() + ".py"
-
-
-def get_module_url():
-    return get_module_location() + "/" + get_module_filename()
-
+from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    tools.download(get_module_url(), get_module_filename(), overwrite=True)
-
-    module = importlib.import_module(get_module_name())
-
-    builder = module.get_builder()
+    builder = build_template_default.get_builder(pure_c=False)
 
     builder.run()
-
-
+    
