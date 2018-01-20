adb31503bc94a10b08e7eb21dfc22968b73c87bf
Jerry W <jerrywiltse@gmail.com>
2018-01-20 01:30:08 -0500

    fix build.py

--- build.py
+++ build.py
@@ -4,5 +4,7 @@
 from bincrafters import build_template_header_only
 
 if __name__ == "__main__":
-    builder = build_template_default.get_builder()
+
+    builder = build_template_header_only.get_builder()
+
     builder.run()
