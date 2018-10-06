6ae52eab9a9d8167ffbf368ec420ad8fffcc61b0
Croydon <cr0ydon@outlook.com>
2018-10-07 00:26:44 +0200

    Readd accidentally removal

--- .ci/install.sh
+++ .ci/install.sh
@@ -19,5 +19,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
-pip install conan_package_tools  bincrafters_package_tools
+pip install conan --upgrade
+pip install conan_package_tools bincrafters_package_tools
+
 conan user
