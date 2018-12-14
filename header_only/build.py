d6958aec87558baf37654a5c47646450f44524e6
Jerry W <jerrywiltse@gmail.com>
2018-12-14 09:24:55 -0500

    try consolidating templates into folders

---
+++ header_only/build.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bincrafters import build_template_header_only
+
+if __name__ == "__main__":
+
+    builder = build_template_header_only.get_builder()
+
+    builder.run()
---
+++ header_only/conanfile.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, tools
+import os
+
+
+class LibnameConan(ConanFile):
+    name = "libname"
+    version = "0.0.0"
+    url = "https://github.com/bincrafters/conan-libname"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    description = "Keep it short"
+    no_copy_source = True
+
+    # Indicates License type of the packaged library
+    license = "MIT"
+
+    # Packages the license for the conanfile.py
+    exports = ["LICENSE.md"]
+
+    # Custom attributes for Bincrafters recipe conventions
+    source_subfolder = "source_subfolder"
+
+    def source(self):
+        source_url = "https://github.com/libauthor/libname"
+        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        extracted_dir = self.name + "-" + self.version
+
+        #Rename to "source_folder" is a convention to simplify later steps
+        os.rename(extracted_dir, self.source_subfolder)
+
+
+    def package(self):
+        include_folder = os.path.join(self.source_subfolder, "include")
+        self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
+        self.copy(pattern="*", dst="include", src=include_folder)
+
+
+    def package_id(self):
+        self.info.header_only()
