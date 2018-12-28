cee545583d3c02dab9a3cc43bafb58093ec975cf
SSE4 <tomskside@gmail.com>
2018-12-28 15:20:57 +0700

    - update header-only template
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- header_only/conanfile.py
+++ header_only/conanfile.py
@@ -8,32 +8,33 @@ import os
 class LibnameConan(ConanFile):
     name = "libname"
     version = "0.0.0"
-    url = "https://github.com/bincrafters/conan-libname"
-    author = "Bincrafters <bincrafters@gmail.com>"
     description = "Keep it short"
+    # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
+    topics = ("conan", "libname", "logging")
+    url = "https://github.com/bincrafters/conan-libname"
+    homepage = "https://github.com/original_author/original_lib"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
     no_copy_source = True
-
-    # Indicates License type of the packaged library
-    license = "MIT"
 
     # Packages the license for the conanfile.py
     exports = ["LICENSE.md"]
 
     # Custom attributes for Bincrafters recipe conventions
-    source_subfolder = "source_subfolder"
+    _source_subfolder = "source_subfolder"
 
     def source(self):
         source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version), sha256="Please-provide-a-checksum")
         extracted_dir = self.name + "-" + self.version
 
         #Rename to "source_folder" is a convention to simplify later steps
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
 
     def package(self):
-        include_folder = os.path.join(self.source_subfolder, "include")
-        self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
+        include_folder = os.path.join(self._source_subfolder, "include")
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
         self.copy(pattern="*", dst="include", src=include_folder)
 
 
