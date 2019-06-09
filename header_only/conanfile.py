58b751f18467fdb6a88edd518a3678306843d170
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-06-09 23:08:35 +0200

    One white line between functions

--- header_only/conanfile.py
+++ header_only/conanfile.py
@@ -30,12 +30,10 @@ class LibnameConan(ConanFile):
         #Rename to "source_folder" is a convention to simplify later steps
         os.rename(extracted_dir, self._source_subfolder)
 
-
     def package(self):
         include_folder = os.path.join(self._source_subfolder, "include")
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
         self.copy(pattern="*", dst="include", src=include_folder)
-
 
     def package_id(self):
         self.info.header_only()
