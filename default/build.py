34752345ac4b54f9ea3e94b340b6c447c514cde0
Croydon <git@cr0ydon.com>
2020-03-06 17:06:07 +0100

    Make pure_c default value more noticeable

--- default/build.py
+++ default/build.py
@@ -4,6 +4,6 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder()
+    builder = build_template_default.get_builder(pure_c=True)
 
     builder.run()
