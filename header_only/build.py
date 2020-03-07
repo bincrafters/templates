35ad5e44165585107e3d007b0cf03bb6469623db
Croydon <git@cr0ydon.com>
2020-03-07 10:51:28 +0100

    Get rid of build.py files
    They can still be used when recipes really need some custom logic, but this should be avoided when possible, since CCI does not support custom build logic
    
    If a build.py exists next to the conanfile.py, the build.py gets executed instead of the autodetection logic

--- header_only/build.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from bincrafters import build_template_header_only
-
-if __name__ == "__main__":
-
-    builder = build_template_header_only.get_builder()
-
-    builder.run()
