b2bf14af9536cc33e497d117cfc497c634fa2f1b
Croydon <git@cr0ydon.com>
2020-03-25 01:01:20 +0100

    Installer test_package: Fix cross-compiling detection
    Reference: https://github.com/conan-io/conan-center-index/pull/1049

--- installer_only/test_package/conanfile.py
+++ installer_only/test_package/conanfile.py
@@ -2,6 +2,8 @@ from conans import ConanFile, tools
 
 class TestPackageConan(ConanFile):
 
+    settings = "os", "os_build"
+
     def test(self):
         if not tools.cross_building(self.settings):
             self.run("some_tool --version")
