901028f1b103853193eb89366756a4bd02bff851
Croydon <git@cr0ydon.com>
2021-02-02 16:18:37 +0100

    Delete fPIC option when shared

--- default/conanfile.py
+++ default/conanfile.py
@@ -24,6 +24,10 @@ class LibnameConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
     def requirements(self):
         self.requires("zlib/1.2.11")
 
