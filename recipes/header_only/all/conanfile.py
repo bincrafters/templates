4e04a03881cfc9857e7519980f5e436b35da26aa
Croydon <git@cr0ydon.com>
2021-07-25 14:23:33 +0200

    Change default URL

--- recipes/header_only/all/conanfile.py
+++ recipes/header_only/all/conanfile.py
@@ -8,7 +8,7 @@ class LibnameConan(ConanFile):
     name = "libname"
     description = "Keep it short"
     topics = ("libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
+    url = "https://github.com/bincrafters/community"
     homepage = "https://github.com/original_author/original_lib"
     license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
     no_copy_source = True
